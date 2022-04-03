68a8e3ed132b9e36a098b587dcb12cff9eea6ad2
SYSTEXUSER1\SYSTEXUSER <brianzheng@systex.com>
2022-04-03 17:15:24 +0800

    tesdt.....

--- test/test/src/main/java/com/example/test/TestApplication.java
+++ test/test/src/main/java/com/example/test/TestApplication.java
@@ -9,7 +9,7 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 		System.out.println("Start spring boot");
-		System.out.println("Start spring boot3");
+		System.out.println("Start spring boot4");
 	}
 
 }
