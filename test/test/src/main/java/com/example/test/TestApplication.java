736795dc80353358dc5bfc089d8c525842ce1cf4
SYSTEXUSER1\SYSTEXUSER <brianzheng@systex.com>
2022-04-03 16:56:29 +0800

    test webhook

--- test/test/src/main/java/com/example/test/TestApplication.java
+++ test/test/src/main/java/com/example/test/TestApplication.java
@@ -9,6 +9,7 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 		System.out.println("Start spring boot");
+		System.out.println("Start spring boot2");
 	}
 
 }
